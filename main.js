2c8eff5d788768f0bd5af49461c147569ce87108
azizbek2OO7 <malikovazizbek726@gmail.com>
2023-03-17 16:26:40 +0500

    added js events

--- main.js
+++ main.js
@@ -5,3 +5,23 @@ const pepsi1 = document.querySelector("#pepsi-1");
 const pepsi2 = document.querySelector("#pepsi-2");
 const pepsi3 = document.querySelector("#pepsi-3");
 
+bluePepsi.addEventListener("click", () => {
+  pepsi1.classList.remove("hidden");
+  pepsi3.classList.add("hidden");
+  pepsi2.classList.add("hidden");
+  document.body.style.background = "#0062be";
+});
+
+whitePepsi.addEventListener("click", () => {
+  pepsi1.classList.add("hidden");
+  pepsi3.classList.add("hidden");
+  pepsi2.classList.remove("hidden");
+  document.body.style.background = "rgb(230,12,45)";
+});
+
+blackPepsi.addEventListener("click", () => {
+  pepsi1.classList.add("hidden");
+  pepsi2.classList.add("hidden");
+  pepsi3.classList.remove("hidden");
+  document.body.style.background = "rgb(31,30,31)";
+});
