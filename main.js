6be736ef23268068b4f644abee43e186da44ec20
Armir <mirarmirkhan@gmail.com>
2023-03-17 16:16:33 +0500

    js seletion

---
+++ main.js
@@ -0,0 +1,7 @@
+const bluePepsi = document.querySelector(".blue-pepsi");
+const whitePepsi = document.querySelector(".white-pepsi");
+const blackPepsi = document.querySelector(".black-pepsi");
+const pepsi1 = document.querySelector("#pepsi-1");
+const pepsi2 = document.querySelector("#pepsi-2");
+const pepsi3 = document.querySelector("#pepsi-3");
+
